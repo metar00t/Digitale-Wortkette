9179f07ec3e6e7aa364ce1b3cf8be0f24b8375d2
David Adams <david-paul.adams@outlook.de>
2025-10-23 12:39:55 +0200

    [Changelog]: Ordnerstruktur an das MVC Pattern angepasst (templates => view)

--- api/v1/dwk/execute/execute.py
+++ api/v1/dwk/execute/execute.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # Hier wird der Main Code stehen, der ausgeführt wird
+@app.route("/api/v1/dwk/home")
+def start():
+    return render_template("landingpage.html")
 
 if __name__ == '__main__':
     app.run(debug = True)

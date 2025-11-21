d9c5bad68a75adb438452e352ded6f653070573c
David Adams <david-paul.adams@outlook.de>
2025-11-21 12:36:38 +0100

    [Update]: Swagger Info und Beschreibung eingebunden. Requirements fuer die Docker-builds angepasst.

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, jsonify, request, redirect, url_for
 from flask_restful import Api, http_status_message
 from flask_swagger import swagger
 
-from Swagger.ClassSpecification.PlayerSpecification import PlayerSpecification
-from Sysfiles.Controller.LobbyController import LobbyController
 from Swagger.ClassSpecification.HostSpecification import HostSpecification
 from Swagger.ClassSpecification.LobbySpecification import LobbySpecification
+from Swagger.ClassSpecification.PlayerSpecification import PlayerSpecification
+from Sysfiles.Controller.LobbyController import LobbyController
 from Sysfiles.Controller.SwaggerController import SwaggerDoc
 
 app = Flask(__name__)
@@ -15,16 +15,27 @@ swaggerInfo.setup(app, Api(app), "/api/v1/dwk/docs", "/api/v1/dwk/spec")
 swaggerInfo.setBlueprint()
 lobbyController = LobbyController()
 
+
 @app.get("/api/v1/dwk/spec")
 def spec():
     swag = swagger(app, from_file_keyword="swagger_from_file")
     swag['info']['version'] = "1.0"
-    swag['info']['title'] = "Digitale Wortkette"
+    swag['info']['title'] = "Digitale Wortkette - FlaskAPI"
+    swag['info']['description'] = "This is the Documentation of the Endpoints for the Flask-API.\nSome useful links:\n- [The Digitale Wortkette Repository](https://github.com/metar00t/Digitale-Wortkette)"
+    swag['info']['contact'] = {
+        "email" : "david-paul.adams@outlook.de"
+    }
+    swag['info']['license'] = {
+        "name" : "Repository License",
+        "url" : "https://github.com/metar00t/Digitale-Wortkette/blob/main/LICENSE"
+    }
     return jsonify(swag)
+
 
 @app.get("/api/v1/dwk/home")
 def home():
     return lobbyController.getLobbyList()
+
 
 @app.route("/api/v1/dwk/host-lobby", methods=['GET', 'POST'])
 def hostLobby():
@@ -34,27 +45,33 @@ def hostLobby():
         lobbyController.saveCurrentLobby()
     return http_status_message(200)
 
+
 @app.get("/api/v1/dwk/join-lobby")
 def joinLobby():
     return redirect(url_for('join', lobbyID=lobbyController.getCurrentLobbyID()))
+
 
 @app.get("/api/v1/dwk/lobby/<int:lobbyID>/lobbySettings")
 def lobbySettings(lobbyID):
     return lobbyController.getChosenLobbySettings(lobbyID)
 
+
 @app.get("/api/v1/dwk/lobby/<int:lobbyID>/playerList")
 def playerList(lobbyID):
     return lobbyController.getPlayerList(lobbyID)
 
+
 @app.post("/api/v1/dwk/lobby/<int:lobbyID>/join")
 def join(lobbyID):
     lobbyController.playerJoins(lobbyID)
-    return {"message" : "Beitritt erfolgreich"}
+    return {"message": "Beitritt erfolgreich"}
+
 
 @app.post("/api/v1/dwk/start-game")
 def startGame():
     lobbyController.setGameStatus()
     return redirect(url_for('game'))
+
 
 @app.route("/api/v1/dwk/game", methods=['GET', 'POST'])
 def game():
@@ -67,9 +84,10 @@ def game():
         else:
             return {}
 
+
 swaggerInfo.addResource(HostSpecification, "/api/v1/dwk/host-lobby")
 swaggerInfo.addResource(LobbySpecification, "/api/v1/dwk/lobby/")
 swaggerInfo.addResource(PlayerSpecification, "/api/v1/dwk/join-lobby")
 
 if __name__ == '__main__':
-    app.run(debug = True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')

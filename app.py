58add50a8bdb2a0065a6d336aa2b32dc6d7ca335
David Adams <david-paul.adams@outlook.de>
2025-11-27 10:31:41 +0100

    [Refactoring]: Labeled currently used Endpoints with their Functionality

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask, jsonify, request, redirect, url_for
 from flask_restful import Api, http_status_message
 from flask_swagger import swagger
 
-from Swagger.ClassSpecification.HomeSpecification import HomeSpecification
-from Swagger.ClassSpecification.HostSpecification import HostSpecification
-from Swagger.ClassSpecification.LobbySettingSpecification import LobbySettingSpecification
-from Swagger.ClassSpecification.LobbySpecification import LobbySpecification
-from Swagger.ClassSpecification.PlayerSpecification import PlayerSpecification
+from Swagger.SwaggerClasses.HomeSpecification import HomeSpecification
+from Swagger.SwaggerClasses.HostSpecification import HostSpecification
+from Swagger.SwaggerClasses.LobbySettingSpecification import LobbySettingSpecification
+from Swagger.SwaggerClasses.LobbySpecification import LobbySpecification
+from Swagger.SwaggerClasses.PlayerSpecification import PlayerSpecification
 from Sysfiles.Controller.LobbyController import LobbyController
 from Sysfiles.Controller.SwaggerController import SwaggerDoc
 
@@ -17,13 +17,14 @@ swaggerInfo.setup(app, Api(app), "/api/v1/dwk/docs", "/api/v1/dwk")
 swaggerInfo.setBlueprint()
 lobbyController = LobbyController()
 
-
+# Swagger Specifications
 @app.get("/api/v1/dwk")
 def spec():
     swag = swagger(app, from_file_keyword="swagger_from_file")
     swag['info']['version'] = "1.0"
     swag['info']['title'] = "Digitale Wortkette - FlaskAPI"
-    swag['info']['description'] = "This is the Documentation of the Endpoints for the Flask-API.\nSome useful links:\n- [The Digitale Wortkette Repository](https://github.com/metar00t/Digitale-Wortkette)"
+    swag['info']['description'] = ("This is the Documentation of the EndpointDefinitions for the Flask-API.\nSome useful links:\n- "
+                                   "[The Digitale Wortkette Repository](https://github.com/metar00t/Digitale-Wortkette)")
     swag['info']['contact'] = {
         "email" : "david-paul.adams@outlook.de"
     }
@@ -33,7 +34,7 @@ def spec():
     }
     return jsonify(swag)
 
-
+# Entrypoint
 @app.get("/api/v1/dwk/home")
 def home():
     if lobbyController.getLobbyList() is None:
@@ -41,31 +42,33 @@ def home():
     else:
         return lobbyController.getLobbyList() , 200
 
-
+# Endpoint for Creating a new Lobby
 @app.route("/api/v1/dwk/host-lobby", methods=['GET', 'POST'])
 def hostLobby():
+    # Create a new Lobby with Default Values
     if request.method == 'GET':
         return lobbyController.createLobby(), 201
+    # Updating the created Lobby with chosen Values
     if request.method == 'POST':
         lobbyController.saveCurrentLobby()
     return http_status_message(200)
 
-
+# TODO: Clarify if this Endpoint is still needed / used
 @app.get("/api/v1/dwk/join-lobby")
 def joinLobby():
     return redirect(url_for('join', lobbyID=lobbyController.getCurrentLobbyID()))
 
-
+# Endpoint for exposing the chosen Lobbysettings for a specified LobbyID
 @app.get("/api/v1/dwk/lobby/<int:lobbyID>/lobbySettings")
 def lobbySettings(lobbyID):
     return lobbyController.getChosenLobbySettings(lobbyID)
 
-
+# Endpoint for exposing the current Playerlist for a specified LobbyID
 @app.get("/api/v1/dwk/lobby/<int:lobbyID>/playerList")
 def playerList(lobbyID):
     return lobbyController.getPlayerList(lobbyID)
 
-
+# Endpoint for joining a Lobby with a given LobbyID
 @app.post("/api/v1/dwk/lobby/<int:lobbyID>/join")
 def join(lobbyID):
     lobbyController.playerJoins(lobbyID)

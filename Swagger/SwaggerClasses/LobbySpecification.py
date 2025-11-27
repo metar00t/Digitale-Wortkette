ce3f8e8a875297a9d7657c427ff74231ce587f28
David Adams <david-paul.adams@outlook.de>
2025-11-27 07:46:37 +0100

    [Swagger]: Renamed Folders for Swagger Clarification Purposes

--- Swagger/SwaggerClasses/LobbySpecification.py
+++ Swagger/SwaggerClasses/LobbySpecification.py
@@ -3,5 +3,5 @@ from flask_restful import Resource
 class LobbySpecification(Resource):
     def get(self):
         """
-        swagger_from_file: ./Swagger/Endpoints/CurrentLobby.yml
+        swagger_from_file: ./Swagger/EndpointDefinitions/CurrentLobby.yml
         """
--- Swagger/SwaggerClasses/PlayerSpecification.py
+++ Swagger/SwaggerClasses/PlayerSpecification.py
@@ -3,5 +3,5 @@ from flask_restful import Resource
 class PlayerSpecification(Resource):
     def post(self):
         """
-        swagger_from_file: ./Swagger/Endpoints/RegisterPlayerToLobby.yml
+        swagger_from_file: ./Swagger/EndpointDefinitions/RegisterPlayerToLobby.yml
         """

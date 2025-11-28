d29f638b9a1094aa36171c5daf6e523d53f16639
David Adams <david-paul.adams@outlook.de>
2025-11-28 11:07:37 +0100

    [Logger]: Changed the format

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ app.logger.setLevel(loglevel)
 
 # Define the Format for Logging
 formatter = logging.Formatter(
-    "%(asctime)s ||%(funcName)s|| %(levelname)s %(message)s",
+    "%(asctime)s [%(funcName)s] %(levelname)s %(message)s",
     datefmt="%d.%m.%Y %H:%M:%S"
 )
 # Set the formatters

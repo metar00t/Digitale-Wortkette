9e8a498b86148b2d8ce83d531d093618c22035b0
David-Paul Adams <david-paul.adams@outlook.de>
2026-02-19 10:59:49 +0100

    [Model]: Updated the Game Model

--- Sysfiles/Model/Game.py
+++ Sysfiles/Model/Game.py
@@ -1,9 +1,25 @@
 class Game:
-    def __init__(self):
-        self.time = 0
+    _maxGameID: int = 0
+    _gameList = []
 
-    def setTime(self,time):
+    def __init__(self):
+        self.gameID = Game._maxGameID + 1
+        Game._maxGameID += 1
+        Game._gameList.append(self.gameID)
+        self.time = 0
+        self.isTimeUp: bool = False
+
+    def setTime(self, time):
         self.time = time
 
     def getTime(self):
         return self.time
+
+    def getGameID(self):
+        return self.gameID
+
+    def callbackTimer(self):
+        self.isTimeUp = True
+
+    def getIsTimeUp(self):
+        return self.isTimeUp
